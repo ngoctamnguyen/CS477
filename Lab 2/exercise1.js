b2b1fea6ffe8832c986b282f688c779f4ff6f9fc
ngoctamnguyen <ngocnguyen@miu.edu>
2022-07-23 06:46:23 -0500

    Lab 2

--- Lab 2/exercise1.js
+++ Lab 2/exercise1.js
@@ -1,5 +1,9 @@
 const dns = require('dns');
 dns.resolve4('www.miu.edu', function(error, address) {
-    if (error) throw error;
-    console.log(address);
+    if (error) {
+        throw new Error('Cannot resovle this domain')
+    } else {
+        console.log(address);
+    }
+   
 });
