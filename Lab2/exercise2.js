e69eaf524437ca7fc2d0cf8a9e4ab2895dc58788
ngoctamnguyen <ngocnguyen@miu.edu>
2022-07-24 18:56:01 -0500

    Lab 3

--- Lab2/exercise2.js
+++ Lab2/exercise2.js
@@ -16,6 +16,6 @@ http.createServer((req, res) => {
     // });
 
     //option3 
-    fs.createReadStream(path.join(__dirname, 'landscape.jpg'), { highWaterMark: 16 * 1024 }).pipe(res);
+    fs.createReadStream(path.join(__dirname, 'card.jpg'), { highWaterMark: 16 * 1024 }).pipe(res);
 
 }).listen(3000, () => console.log('Listening on port 3000'));
